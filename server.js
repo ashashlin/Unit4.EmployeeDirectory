735fd2e229def6b159654fc1e074d3b9cfae7643
Ashley Lin <linashleyy@gmail.com>
2025-05-28 02:39:09 +0800

    Finish everything - move routes into app.js

--- server.js
+++ server.js
@@ -1,37 +1,7 @@
 import app from "#app";
-import employees from "#db/employees";
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello employees!");
-});
-
-app.get("/employees", (req, res) => {
-  res.status(200).json({ employees });
-});
-
-// need to put this above "/employees/:id" bc a request is handled by the first handler with a matching route
-app.get("/employees/random", (req, res) => {
-  const randomIndex = Math.floor(Math.random() * employees.length);
-  const employee = employees[randomIndex];
-
-  res.status(200).json({ employee });
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-  const employee = employees.find((employee) => employee.id === Number(id));
-
-  if (!employee) {
-    return res
-      .status(404)
-      .json({ message: `Employee with id ${id} cannot be found` });
-  }
-
-  res.status(200).json({ employee });
 });
